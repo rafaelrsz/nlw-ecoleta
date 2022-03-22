1d32b027021282ac71e3a23ecbcc9101041bb157
Rafael <rafa.rsouz@gmail.com>
2022-03-22 13:24:45 -0300

    aula 4

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,18 +1,17 @@
-import { Request, Response } from 'express';
-import knex from '../database/connection';
+import { Request, Response } from "express";
+import knex from "../database/connection";
 
 class ItemsController {
   async index(request: Request, response: Response) {
-    const items = await knex('items').select('*');
+    const items = await knex("items").select("*");
 
-    const serializedItems = items.map(item => {
+    const serializedItems = items.map((item) => {
       return {
         id: item.id,
         name: item.name,
-        image_url: `http://localhost:3333/uploads/${item.image}`
-      }
-    }
-    );
+        image_url: `http://192.168.2.180:3333/uploads/${item.image}`,
+      };
+    });
 
     return response.json(serializedItems);
   }
